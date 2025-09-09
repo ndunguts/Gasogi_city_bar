91026a1de8eeeba5fece6bf4ba9607512fb2c092
gitego <gitego@GITEGO.gitego>
2025-09-09 14:12:25 +0200

    add

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,7 +13,7 @@ export default function Home() {
   // Fetch all products
   useEffect(() => {
     axios
-      .get("http://13.60.231.199:8000/api/products/")
+      .get("http://13.60.231.199/api/products/")
       .then((response) => {
         setProducts(response.data);
       })
