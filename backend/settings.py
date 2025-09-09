6ee88db6fb87c269ee6d4cd2443f245f764529de
gitego <gitego@GITEGO.gitego>
2025-09-09 14:22:48 +0200

    add

--- backend/settings.py
+++ backend/settings.py
@@ -8,7 +8,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
+
+
 """
+import os
 
 from pathlib import Path
 
@@ -78,7 +81,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend', 'dist')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
