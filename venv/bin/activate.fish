b7cec007879939f6384cd19cd63565efed9a2359
Ubuntu <ubuntu@ip-172-31-40-88.eu-north-1.compute.internal>
2025-09-11 23:44:36 +0000

    ven

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/gitego/project big/bar/venv'
+set -gx VIRTUAL_ENV /home/ubuntu/project/Gasogi_center_bar/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
