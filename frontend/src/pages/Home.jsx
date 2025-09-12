29f87f6c3c011953a0800a26f63f8a68fd2a51a5
gitego <gitego@GITEGO.gitego>
2025-09-12 03:15:15 +0200

    change responsive

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,7 +13,7 @@ export default function Home() {
   // Fetch all products
   useEffect(() => {
     axios
-      .get("http://16.171.195.132/api/products/")
+      .get("/api/products/")
       .then((response) => {
         setProducts(response.data);
       })
